cb407cb0df4c695d7bdaad2ff88fa38d00396762
ronen-halevy <ronen567@gmail.com>
2023-07-31 18:58:52 +0300

    convert weights - fuse bn and conv, and also save model

--- models/convert_weights.py
+++ models/convert_weights.py
@@ -39,15 +39,18 @@ from models.tf_model import TFModel
 
 
 def run(
-        weights=ROOT / 'yolov5s-seg.pt',  # weights path
+        weights=ROOT / 'yolov5l-seg.pt',  # weights path
         imgsz=(640, 640),  # inference size h,w
         batch_size=1,  # batch size
         dynamic=False,  # dynamic batch size
-        tf_weights_dir='.'
+        tf_weights_dir='.',
+        tf_model_dir='.',
+        **kwargs
 ):
     # PyTorch model
     im = torch.zeros((batch_size, 3, *imgsz))  # BCHW image
-    ref_model = attempt_load(weights, device=torch.device('cpu'), inplace=True, fuse=False)
+    # fuse is essential for porting weights to keras. TBD
+    ref_model = attempt_load(weights, device=torch.device('cpu'), inplace=True, fuse=True)
     _ = ref_model(im)  # inference
     ref_model.info()
     ref_model_seq = ref_model.model
@@ -61,7 +64,12 @@ def run(
     im = keras.Input(shape=(*imgsz, 3), batch_size=None if dynamic else batch_size)
     keras_model = keras.Model(inputs=im, outputs=tf_model.predict(im))
     keras_model.summary()
+
     keras_model.save_weights(tf_weights_dir)
+    # keras_model.trainable=True
+    # tf.keras.models.save_model(keras_model, tf_model_dir)
+    keras_model.save(tf_model_dir)
+
     LOGGER.info('PyTorch, TensorFlow and Keras models successfully verified.\nUse export.py for TF model export.')
     return keras_model, tf_model
 
@@ -72,7 +80,9 @@ def parse_opt():
     parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
     parser.add_argument('--batch-size', type=int, default=1, help='batch size')
     parser.add_argument('--dynamic', action='store_true', help='dynamic batch size')
-    parser.add_argument('--tf_weights_dir', type=str, default='./keras_weights/', help='produced weights target location')
+    parser.add_argument('--tf_weights_dir', type=str, default='./keras_weights/rr.tf', help='produced weights target location')
+    parser.add_argument('--tf_model_dir', type=str, default='./keras_model', help='produced weights target location')
+
     opt = parser.parse_args()
     opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
     print_args(vars(opt))
