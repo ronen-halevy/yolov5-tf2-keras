7c17fbda427198683f3d2e6a576e16b5caad0fe8
ronen-halevy <ronen567@gmail.com>
2023-08-14 16:56:39 +0300

    wip tf train

---
+++ segment/tf_create_dataset.py
@@ -0,0 +1,191 @@
+#! /usr/bin/env python
+# coding=utf-8
+# ================================================================
+#   Copyright (C) 2022 . All rights reserved.
+#
+#   File name   : create_dataset.py
+#   Author      : ronen halevy 
+#   Created date:  10/19/22
+#   Description :
+#
+# ================================================================
+import glob
+from pathlib import Path
+
+# from core.load_tfrecords import parse_tfrecords
+# from core.create_dataset_from_files import create_dataset_from_files
+# from core.load_tfrecords import parse_tfrecords
+
+from utils.tf_general import segments2boxes
+import os
+from PIL import ExifTags, Image, ImageOps
+import contextlib
+import numpy as np
+import tensorflow as tf
+
+IMG_FORMATS = 'bmp', 'dng', 'jpeg', 'jpg', 'mpo', 'png', 'tif', 'tiff', 'webp', 'pfm'  # include image suffixes
+
+for orientation in ExifTags.TAGS.keys():
+    if ExifTags.TAGS[orientation] == 'Orientation':
+        break
+
+
+class LoadImagesAndLabels:
+    def exif_size(self, img):
+        # Returns exif-corrected PIL size
+        s = img.size  # (width, height)
+        with contextlib.suppress(Exception):
+            rotation = dict(img._getexif().items())[orientation]
+            if rotation in [6, 8]:  # rotation 270 or 90
+                s = (s[1], s[0])
+        return s
+    def verify_image_label(self, idx, im_file, lb_file, prefix=''):
+        # Verify one image-label pair
+        # im_file, lb_file, prefix = args
+        nm, nf, ne, nc, msg, segments = 0, 0, 0, 0, '', []  # number (missing, found, empty, corrupt), message, segments
+        try:
+            # verify images
+            im = Image.open(im_file)
+            im.verify()  # PIL verify
+            shape = self.exif_size(im)  # image size
+            assert (shape[0] > 9) & (shape[1] > 9), f'image size {shape} <10 pixels'
+            assert im.format.lower() in IMG_FORMATS, f'invalid image format {im.format}'
+            if im.format.lower() in ('jpg', 'jpeg'):
+                with open(im_file, 'rb') as f:
+                    f.seek(-2, 2)
+                    if f.read() != b'\xff\xd9':  # corrupt JPEG
+                        ImageOps.exif_transpose(Image.open(im_file)).save(im_file, 'JPEG', subsampling=0, quality=100)
+                        msg = f'{prefix}WARNING ⚠️ {im_file}: corrupt JPEG restored and saved'
+
+            # verify labels
+            if os.path.isfile(lb_file):
+                nf = 1  # label found
+                with open(lb_file) as f:
+                    lb = [x.split() for x in f.read().strip().splitlines() if len(x)]
+                    if any(len(x) > 6 for x in lb):  # is segment
+                        classes = np.array([x[0] for x in lb], dtype=np.float32)
+                        img_index = tf.tile(tf.expand_dims(tf.constant([idx]), axis=-1), [classes.shape[0],1])
+                        segments = [np.array(x[1:], dtype=np.float32).reshape(-1, 2) for x in lb]  # (cls, xy1...)
+                        xxx = classes.reshape(-1, 1)
+                        rrr=segments2boxes(segments)
+                        vvv = img_index
+                        lb = np.concatenate( (img_index, classes.reshape(-1, 1), segments2boxes(segments)), 1)  # (cls, xywh)
+                    lb = np.array(lb, dtype=np.float32)
+                nl = len(lb)
+                if nl:
+                    assert lb.shape[1] == 6, f'labels require 6 columns, {lb.shape[1]} columns detected'
+                    assert (lb >= 0).all(), f'negative label values {lb[lb < 0]}'
+                    assert (lb[:,
+                            2:] <= 1).all(), f'non-normalized or out of bounds coordinates {lb[:, 2:][lb[:, 2:] > 1]}'
+                    _, i = np.unique(lb, axis=1, return_index=True)
+                    if len(i) < nl:  # duplicate row check
+                        lb = lb[i]  # remove duplicates
+                        if segments:
+                            segments = [segments[x] for x in i]
+                        msg = f'{prefix}WARNING ⚠️ {im_file}: {nl - len(i)} duplicate labels removed'
+                else:
+                    ne = 1  # label empty
+                    lb = np.zeros((0, 5), dtype=np.float32)
+            else:
+                nm = 1  # label missing
+                lb = np.zeros((0, 5), dtype=np.float32)
+            print('ret', im_file, lb, segments)
+            return im_file, lb, segments
+        except Exception as e:
+            print('!!!!!', e)
+            nc = 1
+            msg = f'{prefix}WARNING ⚠️ {im_file}: ignoring corrupt image/label: {e}'
+            return [None, None, None, None, nm, nf, ne, nc, msg]
+    def img2label_paths(self, img_paths):
+        # Define label paths as a function of image paths
+        sa, sb = f'{os.sep}images{os.sep}', f'{os.sep}labels{os.sep}'  # /images/, /labels/ substrings
+        return [sb.join(x.rsplit(sa, 1)).rsplit('.', 1)[0] + '.txt' for x in img_paths]
+
+    def load(self,
+                 path):
+        # self.img_size = img_size
+        # self.augment = augment
+        # self.hyp = hyp
+        # self.image_weights = image_weights
+        # self.rect = False if image_weights else rect
+        # self.mosaic = self.augment and not self.rect  # load 4 images at a time into a mosaic (only during training)
+        # self.mosaic_border = [-img_size // 2, -img_size // 2]
+        # self.stride = stride
+        self.path = path
+        # self.albumentations = Albumentations(size=img_size) if augment else None
+
+        try:
+            f = []  # image files
+            for p in path if isinstance(path, list) else [path]:
+                p = Path(p)  # os-agnostic
+                if p.is_dir():  # dir
+                    f += glob.glob(str(p / '**' / '*.*'), recursive=True)
+                    # f = list(p.rglob('*.*'))  # pathlib
+                elif p.is_file():  # file
+                    with open(p) as t:
+                        t = t.read().strip().splitlines()
+                        parent = str(p.parent) + os.sep
+                        f += [x.replace('./', parent, 1) if x.startswith('./') else x for x in t]  # to global path
+                        # f += [p.parent / x.lstrip(os.sep) for x in t]  # to global path (pathlib)
+                else:
+                    raise FileNotFoundError(f'{p} does not exist')
+            self.im_files = sorted(x.replace('/', os.sep) for x in f if x.split('.')[-1].lower() in IMG_FORMATS)
+            # self.img_files = sorted([x for x in f if x.suffix[1:].lower() in IMG_FORMATS])  # pathlib
+            assert self.im_files, f'{"prefix"}No images found'
+        except Exception as e:
+            raise Exception(f'Error loading data from {path}: {e}') from e
+
+        self.label_files = self.img2label_paths(self.im_files)  # labels
+        # image_files, lables, segments = zip(*[self.verify_image_label(idx, self.im_file, self.label_file) for idx, (self.im_file, self.label_file) in enumerate(zip(self.im_files, self.label_files))])
+        image_files=[]
+        lables=[]
+        segments=[]
+        for idx, (self.im_file, self.label_file) in enumerate(zip(self.im_files, self.label_files)):
+            image_file, lable, segment = self.verify_image_label(idx, self.im_file, self.label_file)
+            image_files.append(image_file)
+            lables.append(lable)
+            segments.append(segment)
+
+
+
+        return image_files, lables, segments
+
+
+def collate_fn(img, label, path, shapes, masks):
+    # img, label, path, shapes, masks = zip(*batch)  # transposed
+    batched_masks = tf.concat(masks, 0)
+    for i, l in enumerate(label):
+        id = tf.cast(tf.fill([l.shape[0],1], i), tf.float32)
+        l=tf.concat([l, id], axis=-1)
+
+        # l[:, 0] = i  # add target image index for build_targets()
+    return tf.stack(img, 0), tf.concat(label, 0), path, shapes, batched_masks
+
+def decode_and_resize_image(filename, size,  y_lables, y_masks):
+    img_st = tf.io.read_file(filename)
+    img_dec = tf.image.decode_image(img_st, channels=3, expand_animations=False)
+    img = tf.cast(img_dec, tf.float32)
+    # resize w/o keeping aspect ratio - no prob for normal sized images
+    img = tf.image.resize(img/255, size)
+    return img,  y_lables, filename, img.shape, y_masks
+
+def create_dataset(train_path, imgsz):
+    lial = LoadImagesAndLabels()
+    image_files, lables, segments = lial.load(train_path)
+    y_segments = tf.ragged.constant(list(segments))
+    y_lables = tf.ragged.constant(list(lables))
+    x_train = tf.convert_to_tensor(image_files)
+    # img_indices = tf.reshape(tf.convert_to_tensor(range(len(image_files))), (-1,1,1))
+    # print('y_lables',y_lables.shape)
+    # print('img_indices',img_indices.shape)
+
+    # y_lables =tf.concat([y_lables, img_indices], axis=-1)
+
+    dataset = tf.data.Dataset.from_tensor_slices((x_train, y_lables, y_segments))
+    dataset = dataset.map(lambda x, y_lables, y_segments: decode_and_resize_image(x, imgsz,  y_lables, y_segments))
+    return dataset
+    # for img,  y_lables, filename, shape, y_masks in dataset:
+    #     collate_fn(img,  y_lables, filename, img.shape, y_masks )
+    # dataset = dataset.map(lambda img,  y_lables, filename, shape, y_masks : collate_fn(img,  y_lables, filename, shape, y_masks))
+    # for img,  y_lables, filename, img.shape, y_masks in dataset:
+    #     pass
---
+++ utils/segment/tf_loss.py
@@ -0,0 +1,235 @@
+# YOLOv5 🚀 by Ultralytics, AGPL-3.0 license
+"""
+Loss functions
+"""
+
+# import torch
+# import torch.nn as nn
+
+from utils.metrics import bbox_iou
+from utils.torch_utils import de_parallel
+import tensorflow as tf
+
+
+def smooth_BCE(eps=0.1):  # https://github.com/ultralytics/yolov3/issues/238#issuecomment-598028441
+    # return positive, negative label smoothing BCE targets
+    return 1.0 - 0.5 * eps, 0.5 * eps
+
+
+
+class FocalLoss(tf.keras.layers.Layer):#nn.Module):
+    # Wraps focal loss around existing loss_fcn(), i.e. criteria = FocalLoss(nn.BCEWithLogitsLoss(), gamma=1.5)
+    def __init__(self, loss_fcn, gamma=1.5, alpha=0.25):
+        super().__init__()
+        self.loss_fcn = loss_fcn  # must be nn.BCEWithLogitsLoss()
+        self.gamma = gamma
+        self.alpha = alpha
+        self.reduction = loss_fcn.reduction
+        self.loss_fcn.reduction = 'none'  # required to apply FL to each element
+
+    def call(self, pred, true):
+        loss = self.loss_fcn(pred, true)
+        # p_t = torch.exp(-loss)
+        # loss *= self.alpha * (1.000001 - p_t) ** self.gamma  # non-zero power for gradient stability
+
+        # TF implementation https://github.com/tensorflow/addons/blob/v0.7.1/tensorflow_addons/losses/focal_loss.py
+        pred_prob = tf.sigmoid(pred)  # prob from logits
+        p_t = true * pred_prob + (1 - true) * (1 - pred_prob)
+        alpha_factor = true * self.alpha + (1 - true) * (1 - self.alpha)
+        modulating_factor = (1.0 - p_t) ** self.gamma
+        loss *= alpha_factor * modulating_factor
+
+        if self.reduction == 'mean':
+            return loss.mean()
+        elif self.reduction == 'sum':
+            return loss.sum()
+        else:  # 'none'
+            return loss
+
+
+
+
+class ComputeLoss:
+    sort_obj_iou = False
+
+    # Compute losses
+    def __init__(self, hyp, na,nl,nc,anchors, autobalance=False):
+        self.na = na # number of anchors
+        self.nc = nc  # number of classes
+        self.nl = nl  # number of layers
+
+
+        # device = next(model.parameters()).device  # get model device
+        h = hyp  # hyperparameters
+
+        # Define criteria
+        # BCEcls = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([h['cls_pw']], device=device))
+        BCEobj = tf.losses.BinaryCrossentropy(from_logits=True)
+        BCEcls = tf.losses.BinaryCrossentropy(from_logits=True)
+
+        # BCEobj = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([h['obj_pw']], device=device))
+        # BCEcls = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([h['cls_pw']], device=device))
+
+        # Class label smoothing https://arxiv.org/pdf/1902.04103.pdf eqn 3
+        self.cp, self.cn = smooth_BCE(eps=h.get('label_smoothing', 0.0))  # positive, negative BCE targets
+
+        # Focal loss
+        g = h['fl_gamma']  # focal loss gamma
+        if g > 0:
+            BCEcls, BCEobj = FocalLoss(BCEcls, g), FocalLoss(BCEobj, g)
+
+        # m = de_parallel(model).model[-1]  # Detect() module
+        self.balance = {3: [4.0, 1.0, 0.4]}.get(self.nl, [4.0, 1.0, 0.25, 0.06, 0.02])  # P3-P7
+        self.ssi = list(m.stride).index(16) if autobalance else 0  # stride 16 index
+        self.BCEcls, self.BCEobj, self.gr, self.hyp, self.autobalance = BCEcls, BCEobj, 1.0, h, autobalance
+        # self.na = m.na  # number of anchors
+        # self.nc = m.nc  # number of classes
+        # self.nl = m.nl  # number of layers
+        self.anchors = m.anchors
+        # self.device = device
+
+    def __call__(self, p, targets):  # predictions, targets
+        lcls = tf.zeros([1])  # class loss
+        lbox = tf.zeros([1])   # box loss
+        lobj = tf.zeros([1])   # object loss
+        tcls, tbox, indices, anchors = self.build_targets(p, targets)  # targets
+
+        # Losses
+        for i, pi in enumerate(p):  # layer index, layer predictions
+            b, a, gj, gi = indices[i]  # image, anchor, gridy, gridx
+            tobj = tf.zeros([pi.shape[:4]], dtype=pi.dtype)  # target obj
+
+            n = b.shape[0]  # number of targets
+            if n:
+                # pxy, pwh, _, pcls = pi[b, a, gj, gi].tensor_split((2, 4, 5), dim=1)  # faster, requires torch 1.8.0
+                pxy, pwh, _, pcls = pi[b, a, gj, gi].split((2, 2, 1, self.nc), 1)  # target-subset of predictions
+
+                # Regression
+                pxy = pxy.sigmoid() * 2 - 0.5
+                pwh = (pwh.sigmoid() * 2) ** 2 * anchors[i]
+                pbox = tf.concat((pxy, pwh), 1)  # predicted box
+                iou = tf.squeeze(bbox_iou(pbox, tbox[i], CIoU=True))  # iou(prediction, target)
+                lbox += (1.0 - iou).mean()  # iou loss
+
+                # Objectness
+                iou = iou.detach().clamp(0).type(tobj.dtype)
+                if self.sort_obj_iou:
+                    j = iou.argsort()
+                    b, a, gj, gi, iou = b[j], a[j], gj[j], gi[j], iou[j]
+                if self.gr < 1:
+                    iou = (1.0 - self.gr) + self.gr * iou
+                tobj[b, a, gj, gi] = iou  # iou ratio
+
+                # Classification
+                if self.nc > 1:  # cls loss (only if multiple classes)
+                    # t = torch.full_like(pcls, self.cn, device=self.device)  # targets
+                    t = tf.fill(pcls.shape, self.cn)
+                    t[range(n), tcls[i]] = self.cp
+                    lcls += self.BCEcls(pcls, t)  # BCE
+
+                # Append targets to text file
+                # with open('targets.txt', 'a') as file:
+                #     [file.write('%11.5g ' * 4 % tuple(x) + '\n') for x in torch.cat((txy[i], twh[i]), 1)]
+
+            obji = self.BCEobj(pi[..., 4], tobj)
+            lobj += obji * self.balance[i]  # obj loss
+            if self.autobalance:
+                self.balance[i] = self.balance[i] * 0.9999 + 0.0001 / obji.detach().item()
+
+        if self.autobalance:
+            self.balance = [x / self.balance[self.ssi] for x in self.balance]
+        lbox *= self.hyp['box']
+        lobj *= self.hyp['obj']
+        lcls *= self.hyp['cls']
+        bs = tobj.shape[0]  # batch size
+
+        return (lbox + lobj + lcls) * bs, tf.concat((lbox, lobj, lcls))
+
+    def build_targets(self, p, targets):
+        # Build targets for compute_loss(), input targets(image,class,x,y,w,h)
+        na, nt = self.na, targets.shape[0]  # number of anchors, targets
+        tcls, tbox, indices, anch = [], [], [], []
+        gain = tf.ones([7])  # normalized to gridspace gain
+        # ai = torch.arange(na, device=self.device).float().view(na, 1).repeat(1, nt)  # same as .repeat_interleave(nt)
+
+        ai = tf.tile(tf.reshape(tf.range(na, dtype= tf.float32), (na, 1)), [1,nt])
+
+        # targets = torch.cat((targets.repeat(na, 1, 1), ai[..., None]), 2)  # append anchor indices
+        # append anchor indices and targer indices. concat axis 2 to dim 8: im_id, cls, x,y,h,w, ai, ti
+        targets = tf.concat((tf.tile(targets, (na, 1, 1)), ai[..., None]), 2)  # shape: [na, nt,8]
+
+        g = 0.5  # bias
+        # off = torch.tensor(
+        #     [
+        #         [0, 0],
+        #         [1, 0],
+        #         [0, 1],
+        #         [-1, 0],
+        #         [0, -1],  # j,k,l,m
+        #         # [1, 1], [1, -1], [-1, 1], [-1, -1],  # jk,jm,lk,lm
+        #     ],
+        #     device=self.device).float() * g  # offsets
+
+        off = tf.constant(
+            [
+                [0, 0],
+                [1, 0],
+                [0, 1],
+                [-1, 0],
+                [0, -1],  # j,k,l,m
+            ], dtype=tf.float32
+           ) * g  # offsets
+
+        for i in range(self.nl):
+            anchors, shape = self.anchors[i], p[i].shape
+            # gain[2:6] = torch.tensor(shape)[[3, 2, 3, 2]]  # xyxy gain
+            xyxy_gain = tf.constant(shape)[[3, 2, 3, 2]]  # xyxy gain
+            gain = tf.concat((gain[0:2],xyxy_gain ))
+            # gain[2:6] =
+
+
+            # Match targets to anchors
+            t = targets * gain  # shape(3,nt,7)
+            if nt:
+                # Limit ratio between gt boxes and anchors to within threshold (x4 ratio). Otherwise exclude gt box from
+                # unmatching anchor's boxes records.:
+                # Matches:
+                r = t[..., 4:6] / anchors[:, None]  # wh/anchors ratio. shape: [na, nt,2]
+
+                j = tf.math.maximum(r, 1 / r) < self.hyp['anchor_t']  # compare, bool shake: [na, nt]
+
+                # j = wh_iou(anchors, t[:, 4:6]) > model.hyp['iou_t']  # iou(3,n)=wh_iou(anchors(3,2), gwh(n,2))
+                t = t[j]  # filter out unmatched to anchors targets. shape:  [nt, 8] where nt changed to nt_filtered
+
+
+                # idea of offset: pred bbox center trainsformation is pxy.sigmoid()*2-0.5 giving 0.5<=pxy<=1.5 so preds
+                # may transform to adjacent gid squares-upper half of l,u and lower hald of r,d, and need ti be matched
+                # with gt boxes located there. Accordingly, gt boxes set is duplicated 5.  j, k,l,m is true if boxe
+                # center in the left, upp, right, down hald of the square respectively.
+                #
+
+                # Offsets
+                gxy = t[:, 2:4]  # grid xy. shape: [nt,2]
+                gxi = gain[[2, 3]] - gxy  # inverse: offsets from square upper ends. shape: [nt,2]
+                j, k = ((gxy % 1 < g) & (gxy > 1)).T # j,k true if gt in l,up half. gxy<=1 l,u, edge square. shape:[nt].
+                l, m = ((gxi % 1 < g) & (gxi > 1)).T# l,m true if gt in r,low half. gxi<=1 r,l, edge square. shape:[nt].
+                j = tf.stack((tf.ones_like(j), j, k, l, m)) # 5 validity ind to main &4 adjacent squares. shape:[5,nt]
+                t = t.tile((5, 1, 1))[j] # tile by 5 and filter valid. shape: [valid dup nt, 8]
+                offsets = (tf.zeros_like(gxy)[None] + off[:, None])[j] # gt indices offs. shpe:  [valid dup nt, 2]
+            else:
+                t = targets[0]
+                offsets = 0
+
+            # Define
+            bc, gxy, gwh, a = t.chunk(4, 1)  # (image, class), grid xy, grid wh, anchors
+            a, (b, c) = a.long().view(-1), bc.long().T  # anchors, image, class
+            gij = (gxy - offsets).long() # gij=gxy-offs giving left corner of grid square
+            gi, gj = gij.T  # grid indices
+
+            # Append
+            indices.append((b, a, gj.clamp_(0, shape[2] - 1), gi.clamp_(0, shape[3] - 1)))  # image, anchor, grid
+            tbox.append(tf.concat((gxy - gij, gwh), 1))  # xy modified : gxy - gij i.e. box_center-grid square indices
+            anch.append(anchors[a])  # anchors
+            tcls.append(c)  # class
+
+        return tcls, tbox, indices, anch
