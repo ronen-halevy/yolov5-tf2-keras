0aac4e41a7dd535678871250e1c2513634880a97
ronen-halevy <ronen567@gmail.com>
2023-09-02 20:57:44 +0300

    add root path

--- segment/tf_predict.py
+++ segment/tf_predict.py
@@ -34,6 +34,13 @@ import platform
 import sys
 from pathlib import Path
 
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[1]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+
+
 # from models.common import DetectMultiBackend
 from utils.tf_dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadStreams  # , LoadScreenshots, LoadStreams
 
