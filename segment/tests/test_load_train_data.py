80695e3b410dda3df12a7e4cc488c5d9d64ca973
ronen-halevy <ronen567@gmail.com>
2023-09-27 10:26:58 +0300

    remove polygon2masks file  import use config hsv vals

--- segment/tests/test_load_train_data.py
+++ segment/tests/test_load_train_data.py
@@ -25,7 +25,6 @@ ROOT = FILE.parents[1]  # YOLOv5 root directory
 from segment.load_train_data import LoadTrainData
 from segment.tf_create_dataset import CreateDataset
 from utils.tf_general import increment_path
-from utils.segment.polygons2masks import  polygon2mask
 from utils.segment.tf_general import masks2segments, process_mask, process_mask_native
 
 import tensorflow as tf
@@ -121,8 +120,9 @@ def test_dataset_creation(data_path, imgsz=640, line_thickness = 3, nexamples=3,
     mosaic=True# False
     image_files, labels, segments = ltd.load_data(data_path, mosaic)
     degrees, translate, scale, shear, perspective = hyp['degrees'],hyp['translate'],hyp['scale'],hyp['shear'],hyp['perspective']
-    augment=False# True
-    create_dataset = CreateDataset(imgsz, mosaic, augment, degrees, translate, scale, shear, perspective)
+    augment=True
+    hgain, sgain, vgain, flipud, fliplr =hyp['hsv_h'],hyp['hsv_s'],hyp['hsv_v'],hyp['flipud'],hyp['fliplr']
+    create_dataset = CreateDataset(imgsz, mosaic, augment, degrees, translate, scale, shear, perspective, hgain, sgain, vgain, flipud, fliplr)
     ds = create_dataset(image_files, labels, segments)
     # ds = ds.shuffle(10)
     sel_ds = ds.take(nexamples)
