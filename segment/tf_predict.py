c368bd4f5447927a5fe923ce7f1974d207a4512d
ronen-halevy <ronen567@gmail.com>
2023-09-30 14:32:48 +0300

    arrange nms unn predict

--- segment/tf_predict.py
+++ segment/tf_predict.py
@@ -77,6 +77,28 @@ def dir_filelist(images_dir, ext_list='.*'):
     if save_model:
         keras_model.save(model_save_path)
 
+def non_max_suppression(pred, conf_thres,iou_thres, max_det):
+    nm = 32  # number of masks
+    xywh_nbytes = 4
+    obj_nbytes = 1
+    nc = pred.shape[2] - (nm + xywh_nbytes + obj_nbytes)  # number of classes
+    mi = 5 + nc  # mask start index
+    pred = tf.squeeze(pred, axis=0)
+    class_sel_prob = tf.reduce_max(pred[:, 5:mi], axis=-1, keepdims=False)
+    scores = pred[:, 4] * class_sel_prob
+    class_sel_idx = tf.math.argmax(pred[:, 5:mi], axis=-1, output_type=tf.int32)[..., tf.newaxis]
+    class_sel_idx = tf.cast(class_sel_idx, dtype=tf.float32)
+    boxes = xywh2xyxy(pred[:, :4])
+    # ind = [idx  for idx, score in enumerate(scores) if score>0.5]
+
+    ind = tf.image.non_max_suppression(boxes, scores, max_output_size=max_det, iou_threshold=iou_thres,
+                                       score_threshold=conf_thres)
+
+    # Concat before gather:
+    pred = tf.concat((boxes, scores[..., tf.newaxis], class_sel_idx, pred[:, mi:]), axis=1)
+    nms_pred = tf.gather(pred, indices=ind)
+    return nms_pred
+
 
 def run(
         model_cfg_file,
@@ -196,29 +218,10 @@ def run(
             else:
                 pred, proto = keras_model(im) # .predict(im) # To enable bp inside model code, replace .predict(im) by (im)
         #     # NMS
-        nm = 32 # number of masks
-        xywh_nbytes = 4
-        obj_nbytes  =1
-        nc = pred.shape[2] - (nm +xywh_nbytes+ obj_nbytes)  # number of classes
-        mi = 5 + nc  # mask start index
-        pred = tf.squeeze(pred, axis=0)
-        class_sel_prob = tf.reduce_max(pred[:, 5:mi], axis=-1, keepdims=False)
-        scores = pred[:, 4] * class_sel_prob
-        class_sel_idx = tf.math.argmax(pred[:, 5:mi], axis=-1, output_type=tf.int32)[..., tf.newaxis]
-        class_sel_idx = tf.cast(class_sel_idx, dtype=tf.float32)
-        boxes = xywh2xyxy(pred[:, :4])
-        # ind = [idx  for idx, score in enumerate(scores) if score>0.5]
-
-        ind = tf.image.non_max_suppression(boxes, scores, max_output_size=max_det, iou_threshold=iou_thres,
-                                           score_threshold=conf_thres)
-
-        # Concat before gather:
-        pred = tf.concat((boxes, scores[..., tf.newaxis], class_sel_idx, pred[:, mi:]), axis=1)
-        nms_pred = tf.gather(pred, indices=ind)
+        nms_pred=non_max_suppression(pred, conf_thres, iou_thres, max_det)
         b, h, w, ch = tf.cast(im.shape, tf.float32)  # batch, channel, height, width
         nms_pred = nms_pred.numpy()
         nms_pred[..., :4] *= [w, h, w, h]  # xywh normalized to pixels
-
         #
         # with dt[2]:
         #     print('conf_thres',conf_thres)
