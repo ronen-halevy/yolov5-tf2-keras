aff4def0c3b8c8221da065d06437fa738ef8567e
ronen-halevy <ronen567@gmail.com>
2023-12-01 11:03:02 +0200

    change font for colab, del unused methods

--- utils/tf_plots.py
+++ utils/tf_plots.py
@@ -82,7 +82,7 @@ class Annotator:
                 im, data_format=None, scale=True, dtype=None
             )
             self.draw = ImageDraw.Draw(self.im)
-            self.font = check_pil_font(font='Arial.Unicode.ttf' if non_ascii else font,
+            self.font = check_pil_font(font='/usr/share/fonts/truetype/liberation/LiberationSansNarrow-Regular.ttf' if non_ascii else font,
                                        size=font_size or max(round(sum(self.im.size) / 2 * 0.035), 12))
         else:  # use cv2
             self.im = im
@@ -234,68 +234,68 @@ def output_to_target(output, max_det=300):
 
 
 # @threaded
-def plot_images(images, targets, paths=None, fname='images.jpg', names=None):
-    # Plot image grid with labels
-    if isinstance(images, tf.Tensor):
-        images = images.cpu().float().numpy()
-    if isinstance(targets, tf.Tensor):
-        targets = targets.cpu().numpy()
-
-    max_size = 1920  # max image size
-    max_subplots = 16  # max image subplots, i.e. 4x4
-    bs, _, h, w = images.shape  # batch size, _, height, width
-    bs = min(bs, max_subplots)  # limit plot images
-    ns = np.ceil(bs ** 0.5)  # number of subplots (square)
-    if np.max(images[0]) <= 1:
-        images *= 255  # de-normalise (optional)
-
-    # Build Image
-    mosaic = np.full((int(ns * h), int(ns * w), 3), 255, dtype=np.uint8)  # init
-    for i, im in enumerate(images):
-        if i == max_subplots:  # if last batch has fewer images than we expect
-            break
-        x, y = int(w * (i // ns)), int(h * (i % ns))  # block origin
-        im = im.transpose(1, 2, 0)
-        mosaic[y:y + h, x:x + w, :] = im
-
-    # Resize (optional)
-    scale = max_size / ns / max(h, w)
-    if scale < 1:
-        h = math.ceil(scale * h)
-        w = math.ceil(scale * w)
-        mosaic = cv2.resize(mosaic, tuple(int(x * ns) for x in (w, h)))
-
-    # Annotate
-    fs = int((h + w) * ns * 0.01)  # font size
-    annotator = Annotator(mosaic, line_width=round(fs / 10), font_size=fs, pil=True, example=names)
-    for i in range(i + 1):
-        x, y = int(w * (i // ns)), int(h * (i % ns))  # block origin
-        annotator.rectangle([x, y, x + w, y + h], None, (255, 255, 255), width=2)  # borders
-        if paths:
-            annotator.text((x + 5, y + 5), text=Path(paths[i]).name[:40], txt_color=(220, 220, 220))  # filenames
-        if len(targets) > 0:
-            ti = targets[targets[:, 0] == i]  # image targets
-            boxes = xywh2xyxy(ti[:, 2:6]).T
-            classes = ti[:, 1].astype('int')
-            labels = ti.shape[1] == 6  # labels if no conf column
-            conf = None if labels else ti[:, 6]  # check for confidence presence (label vs pred)
-
-            if boxes.shape[1]:
-                if boxes.max() <= 1.01:  # if normalized with tolerance 0.01
-                    boxes[[0, 2]] *= w  # scale to pixels
-                    boxes[[1, 3]] *= h
-                elif scale < 1:  # absolute coords need scale if image scales
-                    boxes *= scale
-            boxes[[0, 2]] += x
-            boxes[[1, 3]] += y
-            for j, box in enumerate(boxes.T.tolist()):
-                cls = classes[j]
-                color = colors(cls)
-                cls = names[cls] if names else cls
-                if labels or conf[j] > 0.25:  # 0.25 conf thresh
-                    label = f'{cls}' if labels else f'{cls} {conf[j]:.1f}'
-                    annotator.box_label(box, label, color=color)
-    annotator.im.save(fname)  # save
+# def plot_images(images, targets, paths=None, fname='images.jpg', names=None):
+#     # Plot image grid with labels
+#     if isinstance(images, tf.Tensor):
+#         images = images.cpu().float().numpy()
+#     if isinstance(targets, tf.Tensor):
+#         targets = targets.cpu().numpy()
+#
+#     max_size = 1920  # max image size
+#     max_subplots = 16  # max image subplots, i.e. 4x4
+#     bs, _, h, w = images.shape  # batch size, _, height, width
+#     bs = min(bs, max_subplots)  # limit plot images
+#     ns = np.ceil(bs ** 0.5)  # number of subplots (square)
+#     if np.max(images[0]) <= 1:
+#         images *= 255  # de-normalise (optional)
+#
+#     # Build Image
+#     mosaic = np.full((int(ns * h), int(ns * w), 3), 255, dtype=np.uint8)  # init
+#     for i, im in enumerate(images):
+#         if i == max_subplots:  # if last batch has fewer images than we expect
+#             break
+#         x, y = int(w * (i // ns)), int(h * (i % ns))  # block origin
+#         im = im.transpose(1, 2, 0)
+#         mosaic[y:y + h, x:x + w, :] = im
+#
+#     # Resize (optional)
+#     scale = max_size / ns / max(h, w)
+#     if scale < 1:
+#         h = math.ceil(scale * h)
+#         w = math.ceil(scale * w)
+#         mosaic = cv2.resize(mosaic, tuple(int(x * ns) for x in (w, h)))
+#
+#     # Annotate
+#     fs = int((h + w) * ns * 0.01)  # font size
+#     annotator = Annotator(mosaic, line_width=round(fs / 10), font_size=fs, pil=True, example=names)
+#     for i in range(i + 1):
+#         x, y = int(w * (i // ns)), int(h * (i % ns))  # block origin
+#         annotator.rectangle([x, y, x + w, y + h], None, (255, 255, 255), width=2)  # borders
+#         if paths:
+#             annotator.text((x + 5, y + 5), text=Path(paths[i]).name[:40], txt_color=(220, 220, 220))  # filenames
+#         if len(targets) > 0:
+#             ti = targets[targets[:, 0] == i]  # image targets
+#             boxes = xywh2xyxy(ti[:, 2:6]).T
+#             classes = ti[:, 1].astype('int')
+#             labels = ti.shape[1] == 6  # labels if no conf column
+#             conf = None if labels else ti[:, 6]  # check for confidence presence (label vs pred)
+#
+#             if boxes.shape[1]:
+#                 if boxes.max() <= 1.01:  # if normalized with tolerance 0.01
+#                     boxes[[0, 2]] *= w  # scale to pixels
+#                     boxes[[1, 3]] *= h
+#                 elif scale < 1:  # absolute coords need scale if image scales
+#                     boxes *= scale
+#             boxes[[0, 2]] += x
+#             boxes[[1, 3]] += y
+#             for j, box in enumerate(boxes.T.tolist()):
+#                 cls = classes[j]
+#                 color = colors(cls)
+#                 cls = names[cls] if names else cls
+#                 if labels or conf[j] > 0.25:  # 0.25 conf thresh
+#                     label = f'{cls}' if labels else f'{cls} {conf[j]:.1f}'
+#                     annotator.box_label(box, label, color=color)
+#     annotator.im.save(fname)  # save
 
 
 def plot_lr_scheduler(optimizer, scheduler, epochs=300, save_dir=''):
@@ -440,34 +440,34 @@ def plot_labels(labels, names=(), save_dir=Path('')):
     plt.close()
 
 
-def imshow_cls(im, labels=None, pred=None, names=None, nmax=25, verbose=False, f=Path('images.jpg')):
-    # Show classification image grid with labels (optional) and predictions (optional)
-    from utils.augmentations import denormalize
-
-    names = names or [f'class{i}' for i in range(1000)]
-    blocks = tf.split(denormalize(im.clone()).cpu().float(), len(im),
-                         dim=0)  # select batch index 0, block by channels
-    n = min(len(blocks), nmax)  # number of plots
-    m = min(8, round(n ** 0.5))  # 8 x 8 default
-    fig, ax = plt.subplots(math.ceil(n / m), m)  # 8 rows x n/8 cols
-    ax = ax.ravel() if m > 1 else [ax]
-    # plt.subplots_adjust(wspace=0.05, hspace=0.05)
-    for i in range(n):
-        ax[i].imshow(blocks[i].squeeze().permute((1, 2, 0)).numpy().clip(0.0, 1.0))
-        ax[i].axis('off')
-        if labels is not None:
-            s = names[labels[i]] + (f'—{names[pred[i]]}' if pred is not None else '')
-            ax[i].set_title(s, fontsize=8, verticalalignment='top')
-    plt.savefig(f, dpi=300, bbox_inches='tight')
-    plt.close()
-    if verbose:
-        LOGGER.info(f'Saving {f}')
-        if labels is not None:
-            LOGGER.info('True:     ' + ' '.join(f'{names[i]:3s}' for i in labels[:nmax]))
-        if pred is not None:
-            LOGGER.info('Predicted:' + ' '.join(f'{names[i]:3s}' for i in pred[:nmax]))
-    return f
-
+# def imshow_cls(im, labels=None, pred=None, names=None, nmax=25, verbose=False, f=Path('images.jpg')):
+#     # Show classification image grid with labels (optional) and predictions (optional)
+#     from utils.augmentations import denormalize
+#
+#     names = names or [f'class{i}' for i in range(1000)]
+#     blocks = tf.split(denormalize(im.clone()).cpu().float(), len(im),
+#                          dim=0)  # select batch index 0, block by channels
+#     n = min(len(blocks), nmax)  # number of plots
+#     m = min(8, round(n ** 0.5))  # 8 x 8 default
+#     fig, ax = plt.subplots(math.ceil(n / m), m)  # 8 rows x n/8 cols
+#     ax = ax.ravel() if m > 1 else [ax]
+#     # plt.subplots_adjust(wspace=0.05, hspace=0.05)
+#     for i in range(n):
+#         ax[i].imshow(blocks[i].squeeze().permute((1, 2, 0)).numpy().clip(0.0, 1.0))
+#         ax[i].axis('off')
+#         if labels is not None:
+#             s = names[labels[i]] + (f'—{names[pred[i]]}' if pred is not None else '')
+#             ax[i].set_title(s, fontsize=8, verticalalignment='top')
+#     plt.savefig(f, dpi=300, bbox_inches='tight')
+#     plt.close()
+#     if verbose:
+#         LOGGER.info(f'Saving {f}')
+#         if labels is not None:
+#             LOGGER.info('True:     ' + ' '.join(f'{names[i]:3s}' for i in labels[:nmax]))
+#         if pred is not None:
+#             LOGGER.info('Predicted:' + ' '.join(f'{names[i]:3s}' for i in pred[:nmax]))
+#     return f
+#
 
 def plot_evolve(evolve_csv='path/to/evolve.csv'):  # from utils.plots import *; plot_evolve()
     # Plot evolve.csv hyp evolution results
