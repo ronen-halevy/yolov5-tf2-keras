0c94c00338bf0abe5e9ed23858d0aaafd9a306a6
ronen-halevy <ronen567@gmail.com>
2023-12-18 12:30:17 +0200

    no change - just expand lines to debug

--- segment/tf_val.py
+++ segment/tf_val.py
@@ -234,7 +234,10 @@ def run(
             if targets.shape[0]: # escape empty targets
                 bindex = tf.cast([idx], tf.float32)[None]
                 bindex = tf.tile(bindex, [targets.shape[0], 1]) # shape: [nt,1] , nt num of example's targets
-                new_targets.extend(tf.concat([bindex, targets.to_tensor()], axis=-1))  #[bindex,cls, xywh] shape: [nt,6]
+                targets_tensor = targets.to_tensor() # ragged to tensor
+                entry = tf.concat([bindex, targets_tensor], axis=-1)
+                new_targets.extend(entry)  #[bindex,cls, xywh] shape: [nt,6]
+
         # list size Nt: [bidx, class, bbox4]-> tensor[Nt, 6]
         batch_targets = tf.stack(new_targets, axis=0) # stack all targets. shape:[Nt,6], Nt sum of all batches targets
 
