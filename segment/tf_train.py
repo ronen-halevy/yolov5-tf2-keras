fc277cbdf078adc7fd189333703f56f125a95f85
ronen-halevy <ronen567@gmail.com>
2024-01-10 23:54:48 +0200

    Input model set to none

--- segment/tf_train.py
+++ segment/tf_train.py
@@ -127,13 +127,13 @@ def train(hyp, opt, callbacks):  # hyp is path/to/hyp.yaml or hyp dictionary
                        ref_model_seq=None, nc=nc, imgsz=imgsz, training=True)
     # im = keras.Input(shape=(*imgsz, 3), batch_size=None if dynamic else batch_size)
     ch=3
-    im = keras.Input(shape=(imgsz[0],imgsz[1], ch), batch_size=None if dynamic else batch_size)
+    im = keras.Input(shape=(None,None, ch), batch_size=None if dynamic else batch_size)
 
     keras_model = tf.keras.Model(inputs=im, outputs=tf_model.predict(im), name='train')
 
     val_tf_model = TFModel(cfg=cfg,
                        ref_model_seq=None, nc=nc, imgsz=imgsz, training=False)
-    im_val = keras.Input(shape=(imgsz[0],imgsz[1], ch), batch_size=None if dynamic else batch_size)
+    im_val = keras.Input(shape=(None,None, ch), batch_size=None if dynamic else batch_size)
 
     val_keras_model = tf.keras.Model(inputs=im_val, outputs=val_tf_model.predict(im_val), name='validation')
 
