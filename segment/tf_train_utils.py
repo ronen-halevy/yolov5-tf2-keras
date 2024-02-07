39d9717bbb1c704b41fd48da296fc8adb62a8a32
ronen-halevy <ronen567@gmail.com>
2024-02-07 22:58:27 +0200

    clean flatten_btargets

--- segment/tf_train_utils.py
+++ segment/tf_train_utils.py
@@ -20,54 +20,15 @@ def flatten_btargets(b_targets, shape0):
         :return: A tensor with image duplicated for all image's targets. shape: [nti], tf.int32
         :rtype:
         """
-        imidx = tf.fill([tf.shape(targets)[0]], idx)
+        imidx = tf.fill([targets.shape[0]], idx)
         return imidx
 
-    def get_ncnt(targets):
-        """
-        Produces image idx with size of nof image's targets.
-        :param target: targets of image i, shape: [nti,5], float.
-        :param idx: index in batch of image i,  tf.int32
-        :return: A tensor with image duplicated for all image's targets. shape: [nti], tf.int32
-        :rtype:
-        """
-        imidx = tf.shape(targets)[0]
-        return imidx
-    im_cnt_targets=[]
-    for tar in b_targets:
-        im_cnt_targets.append(tar.shape[0])
-    # targets_cnt = tf.map_fn(fn=lambda t: get_ncnt(t), elems=(b_targets),
-    #                   fn_output_signature=tf.int32)
-
-    def fill_indices(imidxs, idx):
-        """
-        Produces image idx with size of nof image's targets.
-        :param target: targets of image i, shape: [nti,5], float.
-        :param idx: index in batch of image i,  tf.int32
-        :return: A tensor with image duplicated for all image's targets. shape: [nti], tf.int32
-        :rtype:
-        """
-        imidx = tf.fill([imidxs], idx)
-
-        return imidx
-    acc = [0]
-    summ = 0
-    for cnt in im_cnt_targets:
-        summ+=cnt
-        acc.append(summ)
-    imidxs=tf.ragged.row_splits_to_segment_ids(
-        acc, name=None, out_type=None
-    )
-    # imidxs = tf.map_fn(fn=lambda t: fill_indices(t[0], t[1]), elems=(targets_cnt, idx),
-    #                   fn_output_signature=tf.RaggedTensorSpec(shape=[None], dtype=tf.int32))
-
-    # imidxs=tf.ragged.range(imidxs)
     # generate imidxs - image index for each target. a ragged tensor, shape: [bi, None], int32
-    # idx=tf.reshape(idx, [-1,1]) # does this line prevent unexplained error? TBD
-    # imidxs = tf.map_fn(fn=lambda t: generate_imidx(t[0], t[1]), elems=(b_targets, idx),
-    #                   fn_output_signature=tf.RaggedTensorSpec(shape=[None], dtype=tf.int32))
+    idx=tf.reshape(idx, [-1,1]) # does this line prevent unexplained error? TBD
+    imidxs = tf.map_fn(fn=lambda t: generate_imidx(t[0], t[1]), elems=(b_targets, idx),
+                      fn_output_signature=tf.RaggedTensorSpec(shape=[None], dtype=tf.int32))
     # flatten indices. shape [bnt], i.e. nof targets in batch
-    # imidxs = imidxs.flat_values
+    imidxs = imidxs.flat_values
     # concat imidxs to target. result shape: [bnt, 6]
     imidxs=imidxs[..., None].astype(tf.float32)
     targets = tf.concat([imidxs, targets], axis=-1)
