e202cf9c477deab2c7acdd4a314cf274e9672794
ronen-halevy <ronen567@gmail.com>
2024-03-02 12:19:30 +0200

    comments

--- utils/segment/tf_plots.py
+++ utils/segment/tf_plots.py
@@ -12,10 +12,23 @@ from .. import threaded
 from ..tf_general import xywh2xyxy
 from ..tf_plots import Annotator, colors
 
-
 @threaded
 def plot_images_and_masks(images, targets, masks, paths=None, fname='images.jpg', names=None):
-    # Plot image grid with labels
+    """
+     Plots input images as a grid of subplots, overlayed with masks, bboxes and class annotations.
+     Max number of images in plots' grid  is limmited to [4x4 = 16].
+
+    :param images:  batch of images. shape:[b,640,640,3], tf.float32 [0,1]. Note: pixels can be either normalized or not
+    :param targets: batch's targets. shape: [nt,6] entry: [bi,cls,bbox (normalized)], tf.float32
+    :type masks: shape: [b,160,160], tf.flaot32
+    :param paths: path to images, used for labels. shape: [b] tf str
+    :param fname: output file name
+    :param names: class names list, for label annotations. string (optional)
+    :return:
+    :rtype:
+    """
+
+    # tf to numpy:
     if isinstance(images, tf.Tensor):
         images = images.numpy().astype(np.float32)
     if isinstance(targets, tf.Tensor):
@@ -25,19 +38,18 @@ def plot_images_and_masks(images, targets, masks, paths=None, fname='images.jpg'
     paths = paths.numpy().astype(np.string_)
     max_size = 1920  # max image size
     max_subplots = 16  # max image subplots, i.e. 4x4
-    bs,  h, w, _ = images.shape  # batch size, _, height, width
+    bs,  h, w, _ = images.shape  # unpack shape to batch size, height, width
     bs = min(bs, max_subplots)  # limit plot images
     ns = np.ceil(bs ** 0.5)  # number of subplots in each square's axis (bs=ns*ns)
     if np.max(images[0]) <= 1:
         images *= 255  # de-normalise (optional)
 
-    # Build Image
+    # Fill images into ns*ns mosaic grid:
     mosaic = np.full((int(ns * h), int(ns * w), 3), 255, dtype=np.uint8)  # init ns*ns mosaic
     for i, im in enumerate(images):
         if i == max_subplots:  # limit plotted images
             break
         x, y = int(w * (i // ns)), int(h * (i % ns))  # mosaic sub-image origin: placement order is row after row
-        # im = im.transpose(1, 2, 0)
         mosaic[y:y + h, x:x + w, :] = im
 
     # Resize (optional): rescale if ns*images may exeed mosaic size in either w or h dim.
@@ -54,14 +66,11 @@ def plot_images_and_masks(images, targets, masks, paths=None, fname='images.jpg'
         x, y = int(w * (i // ns)), int(h * (i % ns))  #  mosaic sub-image origin offsets
         annotator.rectangle([x, y, x + w, y + h], None, (255, 255, 255), width=2)  # borders
         if paths[i]:
-            # .name[:40]
             annotator.text((x + 5, y + 5), text=Path(str(paths[i])).name[:40], txt_color=(220, 220, 220))  # filenames
         if targets.shape[0]:
             # targets[:,0] indicates bs id:
             idx = targets[:, 0] == i# mask targets which belong to image i. shape: [Nt]
             ti = targets[idx]  # current image's targets
-            # if ti.shape[0]==0: # no targets in image
-            #     continue
 
             boxes = tf.transpose(xywh2xyxy(ti[:, 2:6]))# shape: [4, nboxes]
             classes = ti[:, 1].astype('int')
@@ -70,16 +79,15 @@ def plot_images_and_masks(images, targets, masks, paths=None, fname='images.jpg'
 
             if boxes.shape[1]: # if nboxes>0, Note that boxes is transposed here
                 if boxes.max() <= 1.01:  # if normalized with tolerance 0.01
-                    # boxes[[0, 2]] *= w  # scale to pixels
-                    # boxes[[1, 3]] *= h
+                    # scale to pixels
                     boxes = tf.concat( [boxes[[0]] * w, boxes[[1]] * h, boxes[[2]] * w, boxes[[3]] * h], axis=0)
                 elif scale < 1:  # absolute coords need scale if image scales
                     boxes *= scale
+            # concat bboxes while adding subimage's origin offset in mosaic to bbox coords:
             boxes = tf.concat([boxes[[0]] + x, boxes[[1]] +y, boxes[[2]] +x, boxes[[3]] +y], axis=0)
 
-            # boxes[[0, 2]] += x # add origin offset in mosaic to current image's bbox xmin, xmax
-            # boxes[[1, 3]] += y  # add origin offset in mosaic to current image's to ymin,ymax
-            for j, box in enumerate(tf.transpose(boxes).tolist()): # loop on boxes, annotate by class name, conf and color
+            # loop on boxes, annotate with class name, conf and color:
+            for j, box in enumerate(tf.transpose(boxes).tolist()):
                 cls = classes[j]
                 color = colors(cls) # select color from list
                 cls = names[cls] if names else cls
@@ -97,7 +105,7 @@ def plot_images_and_masks(images, targets, masks, paths=None, fname='images.jpg'
                     image_masks = np.repeat(image_masks, nti, axis=0) # dup masks per nof labels, shape: [nl,160,160]
                     image_masks = np.where(image_masks == index, 1.0, 0.0) # image_masks holds nti masks colored 1 or 0
                 else: # non-overlap mode or a single target image: mask[i] holds a single target colored by is.
-                    image_masks = masks[idx] # take masks which belong to current image #todo - test overlap with single mask
+                    image_masks = masks[idx] # take masks which belong to current image
                 #  pick color by class value and draw overlay on the image:
                 im = np.asarray(annotator.im).copy()
                 for j, box in enumerate(boxes.T.tolist()):
